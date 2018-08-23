f87c04bb248af04dfdc928eedf7126d9fb108585
JaeCheonKO <jjocojusu1@gmail.com>
2018-08-23 22:39:24 +0900

    기본적인 인텐트 Intent

---
+++ app2/src/main/java/com/study/app2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.study.app2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+// AppCompatActivity : Activity로 부터 상속 받은 class
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 파라미터 : context와 이동할 엑티비티 클래스, Intent는 전달 방법.
+                Intent intent = new Intent(MainActivity.this, secondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
